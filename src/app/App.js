2c1ba8b3b1fe51e370289faafd86aee26dbbb90d
JonathanDev <jonathan.cardozo.dev@gmail.com>
2022-12-14 17:51:20 -0500

    Adding POST and GET

---
+++ src/app/App.js
@@ -0,0 +1,140 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    //  Heredamos del componente
+    super();
+    this.state = {
+      title: "",
+      description: "",
+      tasks: [],
+    };
+    this.addTask = this.addTask.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  addTask(e) {
+    // Capturamos los datos atravez del estado de la aplicacion
+    // console.log(this.state);
+    // Evento fecth para enviar una peticion http al sevidor
+    fetch("/api/task", {
+      method: "POST",
+      body: JSON.stringify(this.state),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      // Aplicamos una promesa
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        M.toast({ html: "Task Saved" });
+        this.setState({ title: "", description: "" });
+        this.getTasks(); // Para mostrar tareas que agregemos
+      })
+      .catch((err) => console.error(err));
+    e.preventDefault();
+  }
+  componentDidMount() {
+    // Al llamar este evento podremos ejecutar cualquier cosa
+    console.log("El componente fue montado");
+    this.getTasks();
+  }
+  getTasks() {
+    fetch("/api/task")
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        this.setState({ tasks: data });
+        // console.log(this.state.tasks);
+      });
+  }
+  handleChange(e) {
+    // Almacenamos en una variable dos datos que estan en e.target
+    const { name, value } = e.target;
+    // Lo usamos para cambiar el estado de una aplicacion de React
+    this.setState({
+      [name]: value,
+    });
+    console.log(e.target.name);
+  }
+  render() {
+    return (
+      <div>
+        <h2>Testing</h2>
+        {/* Navegation */}
+        <nav className="light-blue darken-4">
+          <div className="container">
+            <a href="/" className="brand-logo">
+              MERN STACK
+            </a>
+          </div>
+        </nav>
+        <div className="container">
+          <div className="row">
+            <div className="col s5">
+              <div className="card">
+                <div className="card-content">
+                  <form onSubmit={this.addTask}>
+                    <div className="row">
+                      <div className="input-field col s12">
+                        <input
+                          onChange={this.handleChange}
+                          value={this.state.title}
+                          type="text"
+                          name="title"
+                          id=""
+                          placeholder="Task Title"
+                        />
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="input-field col s12">
+                        <textarea
+                          onChange={this.handleChange}
+                          value={this.state.description}
+                          className="materialize-textarea"
+                          name="description"
+                          id=""
+                          cols="30"
+                          rows="10"
+                          placeholder="Task Description"
+                        ></textarea>
+                      </div>
+                    </div>
+                    <button className="btn light-blue darken-4" type="submit">
+                      Send
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <div className="col s7">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.tasks.map((task) => {
+                    return (
+                      // Cada elemento hijo dentro de un arreglo o iterador deberia tener una propiedad unica
+                      <tr key={task._id}>
+                        <td>{task.title}</td>
+                        <td>{task.description}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/app/index.js
+++ src/app/index.js
@@ -1,18 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import { render } from "react-dom";
 // import ReactDOM from "react-dom/client";
 import { createRoot } from "react-dom/client";
 // const root = ReactDOM.createRoot(document.getElementById("root"))
 const root = createRoot(document.getElementById("app"));
-
+import App from "./App";
 //Esto lo iremos moviendo a archivos independientes a medida que cresca
-class App extends Component {
-  render() {
-    return <h1>Hello World mundo</h1>;
-  }
-}
-
-// alert("react goes here");
 
 // Creamos un componente App y luego lo mostramos en id app
 root.render(<App />);
